ca1ae89a4b0ffc98010b0f2a8dd9a61db0f1f075
Guilherme Z <zamonerguilherme@gmail.com>
2020-11-01 11:02:30 -0300

    add prometheus config

---
+++ src/main/java/br/com/springmicrometerpoc/controller/HelloController.java
@@ -0,0 +1,15 @@
+package br.com.springmicrometerpoc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	
+	@GetMapping("/v1/hello")
+	public String hello() {
+		return "hello";
+	}
+	
+}
