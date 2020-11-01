fc9fe9416c5bb62419915321a06f04c9fece3e27
Guilherme Z <zamonerguilherme@gmail.com>
2020-11-01 10:12:30 -0300

    init

---
+++ src/main/java/br/com/springmicrometerpoc/SpringMicrometerPocApplication.java
@@ -0,0 +1,13 @@
+package br.com.springmicrometerpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicrometerPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMicrometerPocApplication.class, args);
+	}
+
+}
